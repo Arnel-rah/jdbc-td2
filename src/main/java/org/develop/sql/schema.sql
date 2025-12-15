df73f0d5f83b932e37283627222e7aa1563fd5f8
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-15 17:11:59 +0300

    chore: update id team and player to serial primary key

--- src/main/java/org/develop/sql/schema.sql
+++ src/main/java/org/develop/sql/schema.sql
@@ -3,13 +3,13 @@ CREATE TYPE position_enum AS ENUM ('GK', 'DEF', 'MIDF', 'STR');
 CREATE TYPE continent_enum AS ENUM ('AFRICA', 'EUROPA', 'ASIA', 'AMERICA');
 
 CREATE TABLE Team(
-                     id INT PRIMARY KEY,
+                     id SERIAL PRIMARY KEY ,
                      name VARCHAR(255),
                      continent continent_enum
 );
 
 CREATE TABLE Player(
-                       id INT PRIMARY KEY,
+                       id SERIAL PRIMARY KEY,
                        name VARCHAR(255),
                        age INT,
                        position position_enum,

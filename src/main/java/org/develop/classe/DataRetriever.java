6c88d6c95d08e22af61141563c5c129ae8634175
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-15 17:08:38 +0300

    feat(DataRetriever): add skeleton methods for team and player operations

--- src/main/java/org/develop/classe/DataRetriever.java
+++ src/main/java/org/develop/classe/DataRetriever.java
@@ -1,5 +1,33 @@
 package org.develop.classe;
 
+import java.util.List;
+
 public class DataRetriever {
+    Team findTeamById(Integer id){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    List<Player> findPlayers(int page, int size){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    List<Player> createPlayers(List<Player> newPlayers){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    Team saveTeam(Team teamToSave){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    List<Team> findTeamsByPlayerName(String playerName){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    List<Player> findPlayersByCriteria(String playerName,
+                                       PlayerPositionEnum position, String teamName, ContinentEnum continent, int page, int size){
+            throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
 
 }

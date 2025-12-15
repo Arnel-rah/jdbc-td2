c78edd7122b8c86b7ed81c081887dd942c59f472
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-15 16:23:43 +0300

    feat: add SQL schema and seed data files

--- src/main/java/org/develop/sql/db.sql
+++ src/main/java/org/develop/sql/db.sql
@@ -1,11 +1,9 @@
--- create user mini_football_db_manager
+
 CREATE USER mini_football_db_manager WITH PASSWORD '123456';
 
---  grant privileges on the database
 GRANT ALL PRIVILEGES ON DATABASE mini_football_db TO mini_football_db_manager;
 
---  allow user to create tables
 GRANT CREATE ON DATABASE mini_football_db TO mini_football_db_manager;
---  give CRUD permissions on all future tables
+
 ALTER DEFAULT PRIVILEGES
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO mini_football_db_manager;

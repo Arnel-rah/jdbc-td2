493aa90b2f47ba1b079e391cd2c5b32ea5f8ddce
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-01-18 20:16:22 +0300

    feat: add UnitEnum for ingredient quantity measurement

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -2,13 +2,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DBConnection {
 
     public Connection getConnection() {
+        Dotenv dotenv = Dotenv.load();
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
+            String jdbcURl = dotenv.get("JDBC_URl"); //
+            String user = dotenv.get("USER"); //mini_dish_db_manager
+            String password = dotenv.get("PASSWORD"); //123456
             return DriverManager.getConnection(jdbcURl, user, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);

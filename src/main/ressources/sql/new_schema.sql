351268d710add51945fb6f03f4f0ed5ed8afb14d
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-01-18 17:52:15 +0300

    feat: normalize database schema and implement Many-to-Many relationship

--- src/main/ressources/sql/new_schema.sql
+++ src/main/ressources/sql/new_schema.sql
@@ -1,21 +1,26 @@
+CREATE TYPE dish_type AS ENUM ('STARTER', 'MAIN', 'DESSERT'); -- 
+CREATE TYPE ingredient_category AS ENUM ('VEGETABLE', 'MEAT', 'FRUIT', 'DAIRY', 'OTHER'); 
+CREATE TYPE unit_type AS ENUM ('PCS', 'KG', 'L');
+
 CREATE TABLE dish (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    dish_type VARCHAR(50) NOT NULL,
-    selling_price DOUBLE PRECISION
+    name VARCHAR(255) NOT NULL,
+    dish_type dish_type NOT NULL,
+    selling_price NUMERIC(10, 2)
 );
+
 
 CREATE TABLE ingredient (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    category VARCHAR(50) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL, 
+    category ingredient_category NOT NULL 
 );
 
-CREATE TABLE dish_ingredient (
-    dish_id INT REFERENCES dish(id),
-    ingredient_id INT REFERENCES ingredient(id),
-    quantity DOUBLE PRECISION NOT NULL,
-    unit VARCHAR(10) NOT NULL,
-    PRIMARY KEY (dish_id, ingredient_id)
+CREATE TABLE DishIngredient (
+    id SERIAL PRIMARY KEY, 
+    id_dish INT REFERENCES dish(id),
+    id_ingredient INT REFERENCES ingredient(id),
+    quantity_required NUMERIC(10, 2) NOT NULL,
+    unit unit_type NOT NULL 
 );

bd8f239ca69b1e3993e40026a03b3fa7ee3d33e2
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-01-21 18:17:46 +0300

    test: display dish names and gross margins directly in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        DataRetriever data = new DataRetriever();
-        System.out.println(data.findIngredientsByDishId(1));
-}
+        DataRetriever dr = new DataRetriever();
+
+        Dish d1 = dr.findDishById(1);
+        System.out.println(d1.getName() + " " + d1.getGrossMargin());
+
+        Dish d2 = dr.findDishById(2);
+        System.out.println(d2.getName() + " " + d2.getGrossMargin());
+
+        Dish d3 = dr.findDishById(3);
+        System.out.println(d3.getName() + " " + d3.getGrossMargin());
+    }
 }

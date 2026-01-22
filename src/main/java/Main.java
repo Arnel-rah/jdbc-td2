c7df4146dd251edf320e9dbb90d6c78146a19e0d
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-01-22 07:01:14 +0300

    fix: fix bug of test in Main with try catch block

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         DataRetriever dr = new DataRetriever();
 
-        System.out.println(dr.findDishIngredientsByDishId(1));
+        // System.out.println(dr.findDishIngredientsByDishId(1));
 
         Dish d1 = dr.findDishById(1);
         System.out.println(d1.getName() + " " + d1.getGrossMargin());
@@ -10,7 +10,11 @@ public class Main {
         Dish d2 = dr.findDishById(2);
         System.out.println(d2.getName() + " " + d2.getGrossMargin());
 
-        Dish d3 = dr.findDishById(3);
+        try{
+            Dish d3 = dr.findDishById(3);
         System.out.println(d3.getName() + " " + d3.getGrossMargin());
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException" + e.getMessage());
+        }
     }
 }

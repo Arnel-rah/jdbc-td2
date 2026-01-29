01e394cde6235be019521503daf6cbaf6ce79860
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-01-29 09:59:56 +0300

    refactor(Order): clean up validation logic and remove unused imports

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,78 +1,27 @@
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
-    
-    private int id;
-    private String refence;
+    private Integer id;
+    private String reference;
     private Instant creationDatetime;
-    private List<DishOrder> dishOrders;
+    private List<DishOrder> dishOrderList;
 
-
-    public Order(){
-
-    }
-    
-    @Override
-    public String toString() {
-        return "Order [id=" + id + ", refence=" + refence + ", creationDatetime=" + creationDatetime + ", dishOrders="
-                + dishOrders + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id;
-        result = prime * result + ((refence == null) ? 0 : refence.hashCode());
-        result = prime * result + ((creationDatetime == null) ? 0 : creationDatetime.hashCode());
-        result = prime * result + ((dishOrders == null) ? 0 : dishOrders.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Order other = (Order) obj;
-        if (id != other.id)
-            return false;
-        if (refence == null) {
-            if (other.refence != null)
-                return false;
-        } else if (!refence.equals(other.refence))
-            return false;
-        if (creationDatetime == null) {
-            if (other.creationDatetime != null)
-                return false;
-        } else if (!creationDatetime.equals(other.creationDatetime))
-            return false;
-        if (dishOrders == null) {
-            if (other.dishOrders != null)
-                return false;
-        } else if (!dishOrders.equals(other.dishOrders))
-            return false;
-        return true;
-    }
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public String getRefence() {
-        return refence;
+    public String getReference() {
+        return reference;
     }
 
-    public void setRefence(String refence) {
-        this.refence = refence;
+    public void setReference(String reference) {
+        this.reference = reference;
     }
 
     public Instant getCreationDatetime() {
@@ -83,11 +32,36 @@ public class Order {
         this.creationDatetime = creationDatetime;
     }
 
-    public List<DishOrder> getDishOrders() {
-        return dishOrders;
+    public List<DishOrder> getDishOrderList() {
+        return dishOrderList;
     }
 
-    public void setDishOrders(List<DishOrder> dishOrders) {
-        this.dishOrders = dishOrders;
+    public void setDishOrderList(List<DishOrder> dishOrderList) {
+        this.dishOrderList = dishOrderList;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", reference='" + reference + '\'' +
+                ", creationDatetime=" + creationDatetime +
+                ", dishOrderList=" + dishOrderList +
+                '}';
+    }
+
+    Double getTotalAmountWithoutVat() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(id, order.id) && Objects.equals(reference, order.reference) && Objects.equals(creationDatetime, order.creationDatetime) && Objects.equals(dishOrderList, order.dishOrderList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, reference, creationDatetime, dishOrderList);
     }
 }

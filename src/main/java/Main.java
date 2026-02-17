e13d28fa5755890d9799aeb832882000fd42f4c1
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-02-17 22:04:37 +0300

    feat: add getDishCost method with database-side processing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,14 @@ public class Main {
 
     Instant t = Instant.parse("2024-02-01T00:00:00Z");
     Integer ingredientId = 1;
+    Integer dishId = 1;
 
     StockValue result = retriever.getStockValue(t, ingredientId);
 
     System.out.println("quantity actuel: " + result.getQuantity());
+
+    Double cost = retriever.getDishCost(dishId);
+    System.out.println("total cost : " + cost);
 }
 
 }
